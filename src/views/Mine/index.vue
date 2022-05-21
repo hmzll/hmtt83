301c330d3552863ce65c71f57379948be9f65cf3
xpzll <xpzll@126.com>
2022-05-21 10:42:03 +0800

    mine基本布局

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div class="profile-box">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+
+.profile-box {
+    height: 140px;
+    background-color: #3296fa;
+}
+
+</style>
